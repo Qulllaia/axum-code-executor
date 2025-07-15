12740fe2411c5e4c327beff0b0d5a06a2c7d6891
Qulllaia <znurock@mail.ru>
2025-07-16 02:46:43 +0300

    rm

--- src/controller/types.rs
+++
@@ -1,8 +0,0 @@
-use serde::de::{Deserialize};
-
-#[derive(Debug)]
-#[derive(serde::Deserialize)]
-pub struct CreateCodeRequest {
-    pub code: Option<String>,
-    pub file_name: Option<String>,
-}
